9336832660c6f0b68f6b795c369988324d2512ef
ling zhou <lingzhoudesign@gmail.com>
2017-07-27 12:09:31 -0400

    big change
    added hover state, disabled some styles

---
+++ browsecards/enlarge.js
@@ -0,0 +1,14 @@
+function main(){
+//hover to enlarge pic
+
+$('#card1').mouseenter(function(){
+	alert("hi");
+
+});
+
+
+
+
+
+};
+$(document).ready(main);
---
+++ browsecards/main.js
@@ -0,0 +1,13 @@
+function main(){
+
+$('#test').mouseenter(function(){
+	$('.pic').toggleClass('.large');
+
+});
+
+
+
+
+
+};
+$(document).ready(main);
